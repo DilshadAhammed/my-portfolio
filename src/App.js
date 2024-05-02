d757555ba8c55f932cd867a5aeeb2ff061e00f01
DILSHAD AHAMMED N <dilshadnakunnath@gmail.com>
2024-05-02 15:38:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import About from  './component/About';
+import Contact from './component/Contact';
+import Navbar from './component/Navbar';
+import Projects from './component/Projects';
+import Skills from './component/Skills';
+import Testimonials from './component/Testimonials';
+
+export default function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      {/* <Testimonials /> */}
+      <Contact />
+    </main>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Exclusive",
+    subtitle: "shopping-app",
+    description:
+      "A simple shopping website UI",
+    image: "./pImages/P1.png",
+    link: "https://dilshadahammed.github.io/shopping-app-test/",
+  },
+
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "MySQL",
+  "HTML, CSS",
+  "C",
+];
